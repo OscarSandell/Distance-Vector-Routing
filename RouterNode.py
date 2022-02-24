f5b7c80a884256b18a7d434b42dea7e7da1bd758
Oscar <oscar.sandell@live.se>
2022-02-24 10:02:26 +0100

    Skickar upp Co-authored-by: Derfla92 <Derfla92@users.noreply.github.com>

--- RouterNode.py
+++ RouterNode.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import GuiTextArea, RouterPacket, F
 from copy import deepcopy
-
+from RouterSimulator import Event
+from RouterPacket import RouterPacket
 class RouterNode():
     myID = None
     myGUI = None
@@ -18,15 +19,30 @@ class RouterNode():
         self.myGUI = GuiTextArea.GuiTextArea("  Output window for Router #" + str(ID) + "  ")
 
         self.costs = deepcopy(costs)
-
+        self.printDistanceTable()
 
     # --------------------------------------------------
     def recvUpdate(self, pkt):
-        pass
+        self.myGUI.println("HEJ " +str(pkt.sourceid))
+
+        #Uppdatera cost på något sätt hmmmmmmmmmmmmm.....
+        tempcosts = self.costs
+
+        for cost in tempcosts:
+        #   dx(y) = minv{c(x,v) + dv(y)}
+            cost = min(cost[self.myID],cost) + (max(self.costs) - cost)
+
+        for cost in tempcosts:
+            if cost != self.costs:
+                for i in range(self.sim.NUM_NODES):
+                    pkt = RouterPacket(self.myID,i,self.costs)
+                    self.sendUpdate(pkt)
+
 
 
     # --------------------------------------------------
     def sendUpdate(self, pkt):
+        
         self.sim.toLayer2(pkt)
 
 
@@ -34,8 +50,14 @@ class RouterNode():
     def printDistanceTable(self):
         self.myGUI.println("Current table for " + str(self.myID) +
                            "  at time " + str(self.sim.getClocktime()))
-
+        self.myGUI.println(str(self.costs))
 
     # --------------------------------------------------
     def updateLinkCost(self, dest, newcost):
+
+        self.myGUI.println(str(newcost))
+
+        
+        
+        
         pass
